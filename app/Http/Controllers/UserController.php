b5c7fa1ee97297f425a6275cdbadbeced179f981
achmadtaufikhidayatullah <achmadtaufikhidayatullah6@gmail.com>
2023-03-14 17:31:28 +0800

    insert HASH di usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
